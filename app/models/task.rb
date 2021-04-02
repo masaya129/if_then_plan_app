babe4b0538b0e3b9f7581613afaea17879b2ed84
masaya129 <mirapani666@gmail.com>
2021-04-02 18:27:44 +0900

    ActiveHash導入

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,13 @@
 class Task < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :unit
   belongs_to :user
 
   with_options presence: true do
     validates :if_task
     validates :then_task
     validates :title
-    validates :unit
+    validates :unit, numericality: { other_than: 1 } 
     validates :month_goal
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'tasks/index'
   root to: "tasks#index"
+  resources :tasks
 end
