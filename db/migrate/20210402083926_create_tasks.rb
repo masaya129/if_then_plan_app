6c0ab727dc37e5883545ffb3d97c128be4671070
masaya129 <mirapani666@gmail.com>
2021-04-02 18:01:18 +0900

    taskモデルのテーブル作成

--- db/migrate/20210402083926_create_tasks.rb
+++ db/migrate/20210402083926_create_tasks.rb
@@ -1,7 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-
+      t.string :if_task,      null: false
+      t.string :then_task,    null: false
+      t.string :title,        null: false
+      t.integer :unit,        null: false
+      t.string  :month_goal,  null: false
+      t.references :user,     null: false, foreign_key: true
       t.timestamps
     end
   end
