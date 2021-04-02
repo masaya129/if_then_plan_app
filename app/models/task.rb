fbe12912f203c477fd15ab6951786ffca4b86ad3
masaya129 <mirapani666@gmail.com>
2021-04-02 18:07:50 +0900

    バリデーション記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :if_task
+    validates :then_task
+    validates :title
+    validates :unit
+    validates :month_goal
+  end
+  
 end
