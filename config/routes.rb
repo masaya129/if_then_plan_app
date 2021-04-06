06b8e7b424f36cc20448c14fe304411bdf201d19
masaya129 <mirapani666@gmail.com>
2021-04-06 14:27:15 +0900

    recordコントローラー、モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'records/index'
   devise_for :users
   root to: "tasks#index"
   resources :tasks, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :records
 end
