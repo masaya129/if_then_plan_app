2b5113989f81c33c9f59a938098e3be5471af8f5
masaya129 <mirapani666@gmail.com>
2021-04-11 14:16:21 +0900

    recordのcreate機能実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,28 +1,29 @@
 class RecordsController < ApplicationController
   def index
-    @records = Record.new
+    @records = Record.all
     @task = Task.find(params[:task_id])
   end
 
   def new
     @task = Task.find(params[:task_id])
-    @record = Record.new
-    records = @task.records.new(record_params)
+    @record = current_user.records.new
+  
   end
 
   def create
     @task = Task.find(params[:task_id])
     @record = @task.records.new(record_params)
     if @record.save
-      redirect_to task_records_path(task.id)
+      redirect_to task_records_path(@task.id)
     else
       render :new
     end
+    
   end
 
   private
 
   def record_params
-    params.permit(:count, :memo, :start_time).merge(user_id: current_user.id, task_id: params[:task_id])
+    params.require(:record).permit(:count, :memo, :start_time).merge(user_id: current_user.id)
   end
 end
