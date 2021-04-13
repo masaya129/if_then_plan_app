3dfc4cab9f5bc3eb09be2fc3ad8d37fe3e66c5d2
masaya129 <mirapani666@gmail.com>
2021-04-13 18:22:34 +0900

    taskモデルのテストファイル作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {'test'}
     email                 {'test@example'}
-    password              {'000011'}
+    password              {'aaaa11'}
     password_confirmation {password}
   end
 end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe '習慣化タスク登録機能', type: :model do
+  before do
+    
+  end
+end
