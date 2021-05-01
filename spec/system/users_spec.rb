120a0d030e0f06880981c7ef9c28956e02b30869
masaya129 <mirapani666@gmail.com>
2021-05-02 01:11:52 +0900

    新規登録異常系テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -39,11 +39,22 @@ RSpec.describe "ユーザー新規登録", type: :system do
   context "ユーザーが新規登録できないとき" do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: ''
+      fill_in 'メールアドレス', with: ''
+      fill_in 'パスワード', with: ''
+      fill_in 'パスワード（確認用）', with: ''
       # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq user_registration_path
     end
   
   end
